7d58f61fee704c35a35880551d10d502d711f52c
Viktor Nagy <viktor.nagy@01people.com>
2022-11-02 14:22:00 +0100

    refactor: demo comment

--- src/modules/socket/socket.gateway.ts
+++ src/modules/socket/socket.gateway.ts
@@ -1,6 +1,6 @@
 import { Logger } from '@nestjs/common';
 import {
-    ConnectedSocket,
+  ConnectedSocket,
   MessageBody,
   OnGatewayInit,
   SubscribeMessage,
@@ -15,10 +15,16 @@ export class SocketGateway implements OnGatewayInit {
     this.logger.log('Websocket Gateway initialized');
   }
 
+  /**
+   *  this event's purpose is to return same data as well
+   *  as emit it to pong event for everyone else
+   *
+   * @author Viktor Nagy <viktor.nagy@01people.com>
+   */
   @SubscribeMessage('ping')
   handleEvent(
     @MessageBody() data: any,
-    @ConnectedSocket() socket: Socket
+    @ConnectedSocket() socket: Socket,
   ): Record<string, any> {
     Logger.log('received ping event', data);
     const response = {
