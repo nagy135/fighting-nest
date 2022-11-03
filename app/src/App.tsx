39423941af98b1d9b1d7c91dfe4eff9d0c4ef091
Viktor Nagy <viktor.nagy@01people.com>
2022-11-03 15:56:48 +0100

    feat: requestAnimationFrame

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,36 +1,71 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const PLAYER_RADIUS = 10;
+const STEP = 10;
 
 type TPosition = {
   x: number;
   y: number;
-}
+};
 
 export default () => {
-
-  const [position, setPosition] = useState<TPosition>({x: 100, y: 100});
+  const positionRef = useRef<TPosition>({ x: 100, y: 100 });
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const playerMove = (key: string) => {
+    switch (key) {
+      case "w":
+        positionRef.current = {
+          x: positionRef.current.x,
+          y: positionRef.current.y - STEP,
+        };
+        break;
+      case "s":
+        positionRef.current = {
+          x: positionRef.current.x,
+          y: positionRef.current.y + STEP,
+        };
+        break;
+      case "a":
+        positionRef.current = {
+          x: positionRef.current.x - STEP,
+          y: positionRef.current.y,
+        };
+        break;
+      case "d":
+        positionRef.current = {
+          x: positionRef.current.x + STEP,
+          y: positionRef.current.y,
+        };
+        break;
+    }
+    console.log(key);
+  };
+
+  const tick = () => {
+    if (!canvasRef.current || !positionRef.current) return;
+    const ctx = canvasRef.current.getContext("2d");
+    if (!ctx) return;
+    const { x, y, width, height } = canvasRef.current.getBoundingClientRect();
+
+    ctx.clearRect(0, 0, width, height);
+
+    ctx.beginPath();
+    ctx.arc(
+      positionRef.current.x,
+      positionRef.current.y,
+      PLAYER_RADIUS,
+      0,
+      2 * Math.PI
+    );
+    ctx.fill();
+    window.requestAnimationFrame(tick);
+  };
+
   useEffect(() => {
-    const handle = setInterval(() => {
-    if (!canvasRef.current) return;
-      const ctx = canvasRef.current.getContext('2d');
-      if (!ctx) return;
-      const { x, y, width, height } = canvasRef.current.getBoundingClientRect();
-
-      ctx.clearRect(0, 0, width, height);
-
-      ctx.beginPath();
-      ctx.arc(position.x, position.y, PLAYER_RADIUS, 0, 2*Math.PI);
-      ctx.fill();
-
-    }, 1000);
-
-    return () => clearInterval(handle);
+    window.addEventListener("keydown", (e) => playerMove(e.key));
+    window.requestAnimationFrame(tick);
   }, []);
-  return (
-    <canvas id="canvas" ref={canvasRef} width={300} height={300}>
-    </canvas>
-  );
-}
+  return <canvas id="canvas" ref={canvasRef} width={300} height={300}></canvas>;
+};
