74dfb50174d9595139e735ecc05912b3b5b9f778
Viktor Nagy <viktor.nagy@01people.com>
2022-11-03 15:41:41 +0100

    feat: first demo canvas react app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PLAYER_RADIUS = 10;
+
+type TPosition = {
+  x: number;
+  y: number;
 }
 
-export default App;
+export default () => {
+
+  const [position, setPosition] = useState<TPosition>({x: 100, y: 100});
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const handle = setInterval(() => {
+    if (!canvasRef.current) return;
+      const ctx = canvasRef.current.getContext('2d');
+      if (!ctx) return;
+      const { x, y, width, height } = canvasRef.current.getBoundingClientRect();
+
+      ctx.clearRect(0, 0, width, height);
+
+      ctx.beginPath();
+      ctx.arc(position.x, position.y, PLAYER_RADIUS, 0, 2*Math.PI);
+      ctx.fill();
+
+    }, 1000);
+
+    return () => clearInterval(handle);
+  }, []);
+  return (
+    <canvas id="canvas" ref={canvasRef} width={300} height={300}>
+    </canvas>
+  );
+}
