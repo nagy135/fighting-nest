5b71d43f9262c87e65447bc6799a8f363513e8ae
Viktor Nagy <viktor.nagy@01people.com>
2022-11-04 10:40:00 +0100

    feat: attack, constants, basic fighting demo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,12 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
 const PLAYER_RADIUS = 10;
+const HEALTH_BAR_OFFSET_Y = -PLAYER_RADIUS - 10;
+const HEALTH_BAR_OFFSET_X = -PLAYER_RADIUS;
+const HEALTH_BAR_WIDTH = 5;
+const DEFAULT_HEALTH = 100;
+const ATTACKING_FRAMES = 5;
+const ATTACK_LINE_WIDTH = 4;
 const STEP = 10;
 
 type TPosition = {
@@ -10,6 +16,8 @@ type TPosition = {
 
 export default () => {
   const positionRef = useRef<TPosition>({ x: 100, y: 100 });
+  const attackingRef = useRef<number>(0);
+  const healthRef = useRef<number>(DEFAULT_HEALTH);
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -39,8 +47,9 @@ export default () => {
           y: positionRef.current.y,
         };
         break;
+      case " ":
+        attackingRef.current = ATTACKING_FRAMES;
     }
-    console.log(key);
   };
 
   const tick = () => {
@@ -60,6 +69,27 @@ export default () => {
       2 * Math.PI
     );
     ctx.fill();
+
+    ctx.fillRect(
+      positionRef.current.x + HEALTH_BAR_OFFSET_X,
+      positionRef.current.y + HEALTH_BAR_OFFSET_Y,
+      PLAYER_RADIUS*2,
+      HEALTH_BAR_WIDTH
+    )
+
+    if (attackingRef.current) {
+      ctx.beginPath();
+      ctx.lineWidth = ATTACK_LINE_WIDTH;
+      ctx.arc(
+        positionRef.current.x,
+        positionRef.current.y,
+        PLAYER_RADIUS * 2 * (ATTACKING_FRAMES - attackingRef.current),
+        0,
+        2 * Math.PI
+      );
+      attackingRef.current -= 1;
+      ctx.stroke();
+    }
     window.requestAnimationFrame(tick);
   };
 
@@ -67,5 +97,5 @@ export default () => {
     window.addEventListener("keydown", (e) => playerMove(e.key));
     window.requestAnimationFrame(tick);
   }, []);
-  return <canvas id="canvas" ref={canvasRef} width={300} height={300}></canvas>;
+  return <canvas id="canvas" ref={canvasRef} width={300} height={300} />;
 };
